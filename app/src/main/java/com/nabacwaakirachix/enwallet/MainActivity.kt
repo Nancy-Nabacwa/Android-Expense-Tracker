4fba2e8a50de8795df61f50f881621dbf254878a
Nancy-Nabacwa <nancypaulinenabacwa@gmail.com>
2024-07-08 09:09:28 +0300

    added expense tracker

---
+++ app/src/main/java/com/nabacwaakirachix/enwallet/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nabacwaakirachix.enwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nabacwaakirachix.enwallet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
